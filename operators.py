62c4c46bbd5bea5b29f4b4030ceb674b2d9fe147
Stefan Fernández Hernández <stefanfernandez019@gmail.com>
2026-01-02 20:07:06 +0100

    operators in python

---
+++ operators.py
@@ -0,0 +1,22 @@
+print('Arithmetic operators are symbols used to perform basic mathematical operations on numbers (+, -, *, /, **, //, %).')
+print()
+
+print('For example:     5+7=', 5+7, '    50/3=', 50/3)
+print()
+
+print('Relational operators are used to compare two values (==, !=, >, <, >=, <=), they return a boolean value: True or False.')
+print()
+
+print('For example:    5>=4', 5>=4,'     ' '19!=17', 19!=17)
+print()
+
+print('Logical operators are used to combine or negate boolean expressions(not, or, and), they always return a boolean value.')
+
+a=10
+b=5
+
+print()
+print('a = 10  b=5')
+print('a > 5 and b > 3  ', a>5 and b>3)
+print('a < 5 or b > 10  ', a<5 or b>10)
+print('not (a > 5)      ', not (a>5))
